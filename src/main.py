6260269536c76905fe226962cb9d429e0b51688f
Oshank <oshankashyap@protonmail.com>
2023-01-21 08:35:36 +0530

    implemented checkConfig() function to check whether the TLP config file is present or not | If the file is not present then it will create a new one with the default configuration

--- src/main.py
+++ src/main.py
@@ -1,10 +1,45 @@
 import time
 import platform
 import attributes
+from pathlib import Path
+
+
+def checkConfig():
+    # function to check whether the configuration file for TLP exists
+    # if not create a new file
+
+    path = Path("/etc/tlp.d/00-test.conf")
+    if not path.exists():
+        contents = [
+            "# Test File",
+            "TLP_ENABLE=1",
+            "",
+            "# Battery Configuration",
+            "#START_CHARGE_THRESH_BAT0=0",
+            "#STOP_CHARGE_THRESH_BAT0=0",
+            "",
+            "# CPU Configuration",
+            "#CPU_MIN_PERF_ON_AC=10",
+            "#CPU_MAX_PERF_ON_AC=100",
+            "#CPU_MIN_PERF_ON_BAT=10",
+            "#CPU_MAX_PERF_ON_BAT=37",
+            "",
+            "CPU_BOOST_ON_AC=0",
+            "CPU_BOOST_ON_BAT=0",
+            "",
+            "CPU_SCALING_GOVERNOR_ON_AC=powersave",
+            "CPU_SCALING_GOVERNOR_ON_BAT=powersave",
+        ]
+
+        with open(path, "w") as fileObj:
+            for x in contents:
+                fileObj.write(f"{x}\n")
+
 
 if __name__ == "__main__":
     PROCESSOR = platform.processor()
 
+    checkConfig()
     attributes.bat.checkThresholdSupport()
     attributes.bat.checkPercentage()
 
